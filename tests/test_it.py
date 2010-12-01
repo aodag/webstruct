3afef3643d1aebc32568002b03386c912edb2884
Atsushi Odagiri <aodagx@gmail.com>
2010-12-01 21:33:03 +0900

    simple dispatch

--- tests/test_it.py
+++ tests/test_it.py
@@ -6,16 +6,25 @@ here = os.path.dirname(__file__)
 
 class TestApplication(unittest.TestCase):
     
-    def test_init_app(self):
+    def test_simple_app(self):
         import webstruct
         class Application(webstruct.Application):
             templates = [os.path.join(here, 'templates')]
 
-            @webstruct.conf(default=True, template='index.html')
+            @webstruct.view(default=True, template='index.html')
             def index(request):
-                return {}
+                return {'message': 'This is Top'}
+
+            @webstruct.view(default=True, template='index.html')
+            def greeting(request):
+                return {'message': 'Hello, world!'}
+
         app = TestApp(Application)
         res = app.get('/')
         self.assertEqual(res.status_int, 200)
+        self.assertEqual(res.body, "This is Top")
+        res = app.get('/greeting')
+        self.assertEqual(res.body, "Hello, world!")
+
 
 
--- webstruct/__init__.py
+++ webstruct/__init__.py
@@ -1,4 +1,6 @@
 from webob import Request, Response
+from webob.exc import *
+from jinja2 import Environment, FileSystemLoader
 
 class ApplicationType(object):
     """ wsgi application type
@@ -7,10 +9,23 @@ class ApplicationType(object):
         self.name = name
         self.bases = bases
         self.dct = dct
+        environment = Environment(loader=FileSystemLoader(dct['templates']))
+        for name, value in dct.iteritems():
+            if getattr(value, 'is_view', False):
+                value.func.jinja2_environment = environment
 
     def __call__(self, environ, start_response):
         req = Request(environ)
-        res = Response()
+        path = req.path_info_pop()
+        if path is None or path == '':
+            path = 'index'
+        for name, func in self.dct.iteritems():
+            if callable(func) and name == path:
+                
+                res = req.get_response(func)
+                break
+        else:
+            res = HTTPNotFound()
         return res(environ, start_response)
 
 
@@ -20,11 +35,23 @@ class Application(object):
 
     """
     __metaclass__ = ApplicationType
+    templates = []
 
-def conf(**config):
+import functools
+
+def view(**config):
     """ view func configurator
     """
     def dec(func):
-        return func
+        @functools.wraps(func)
+        def wrap(environ, start_response):
+            request = Request(environ)
+            data = func(request)
+            tmpl = func.jinja2_environment.get_template(config['template'])
+            response = Response(tmpl.render(data))
+            return response(environ, start_response)
+        wrap.func = func
+        wrap.is_view = True
+        return wrap
     return dec
 
